816a0dd7e0ccf035413d725ece71f022a176c3c6
Tharun Mohan <tharun.mohan@bossinfo.ch>
2024-11-18 10:59:52 +0100

    Made new game changes and changes to the atlas map 2

--- cyber_apocalypse/lib/ui/neu_game_screen.dart
+++ cyber_apocalypse/lib/ui/neu_game_screen.dart
@@ -29,118 +29,119 @@ class _NeuGameScreenState extends State<NeuGameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false, // Hier die Anpassung
       body: Material(
-      child: Center(
-        child: AspectRatio(
-          aspectRatio: 9 / 19.5,
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/ui/background1.png'),
-                fit: BoxFit.cover,
+        child: Center(
+          child: AspectRatio(
+            aspectRatio: 9 / 19.5,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/ui/background1.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: LayoutBuilder(builder: (context, constrains) {
-                return Stack(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 24),
-                        Image.asset('assets/ui/title.png'),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Name',
-                                  hintStyle: TextStyle(color: Colors.white),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: LayoutBuilder(builder: (context, constrains) {
+                  return Stack(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 24),
+                          Image.asset('assets/ui/title.png'),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                TextField(
+                                  decoration: InputDecoration(
+                                    hintText: 'Name',
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.white),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.white),
+                                    ),
                                   ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.white),
-                                  ),
+                                  style: TextStyle(color: Colors.white),
+                                  onChanged: (value) {
+                                    if (value.isNotEmpty) {
+                                      playerName = value;
+                                      nameFilld = true;
+                                    } else {
+                                      nameFilld = false;
+                                    }
+                                  },
                                 ),
-                                style: TextStyle(color: Colors.white),
-                                onChanged: (value) {
-                                  if (value.isNotEmpty) {
-                                    playerName = value;
-                                    nameFilld = true;
-                                  } else {
-                                    nameFilld = false;
-                                  }
-                                },
-                              ),
-                              Container(
-                                  margin: EdgeInsets.only(
-                                      left: 15, bottom: 30, top: 40),
-                                  child: Row(
-                                    children: [
-                                      IconButton(
-                                          iconSize: 70,
-                                          onPressed: () => {
-                                                context
-                                                    .read<CharacterProvider>()
-                                                    .changeCharacter()
-                                              },
-                                          icon: Icon(Icons.arrow_left),
-                                          color: Colors.white),
-                                      Consumer<CharacterProvider>(
-                                        builder: (context, characterProvider,
-                                            child) {
-                                          return SpriteWidget(
-                                            sprite: characterProvider.isSonic
-                                                ? Assets.sonicJump
-                                                : Assets.shadowJump,
-                                          );
-                                        },
-                                      ),
-                                      IconButton(
-                                          iconSize: 70,
-                                          onPressed: () => {
-                                                context
-                                                    .read<CharacterProvider>()
-                                                    .changeCharacter()
-                                              },
-                                          icon: Icon(Icons.arrow_right),
-                                          color: Colors.white),
-                                    ],
-                                  )),
-                              MyButton(
-                                'Spielen',
-                                onPressed: () => {
-                                  if (nameFilld)
-                                    {
-                                      context.read<CharacterProvider>().setPlayerName(playerName),
-                                      context.pushAndRemoveUntil(Routes.game)
-                                    }
-                                  else
-                                    {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(SnackBar(
-                                        content: Text('Bitte gib ein Name ein'),
-                                      ))
-                                    }
-                                },
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                );
-              }),
+                                Container(
+                                    margin: EdgeInsets.only(
+                                        left: 15, bottom: 30, top: 40),
+                                    child: Row(
+                                      children: [
+                                        IconButton(
+                                            iconSize: 70,
+                                            onPressed: () => {
+                                                  context
+                                                      .read<CharacterProvider>()
+                                                      .changeCharacter()
+                                                },
+                                            icon: Icon(Icons.arrow_left),
+                                            color: Colors.white),
+                                        Consumer<CharacterProvider>(
+                                          builder: (context, characterProvider,
+                                              child) {
+                                            return SpriteWidget(
+                                              sprite: characterProvider.isSonic
+                                                  ? Assets.sonicJump
+                                                  : Assets.shadowJump,
+                                            );
+                                          },
+                                        ),
+                                        IconButton(
+                                            iconSize: 70,
+                                            onPressed: () => {
+                                                  context
+                                                      .read<CharacterProvider>()
+                                                      .changeCharacter()
+                                                },
+                                            icon: Icon(Icons.arrow_right),
+                                            color: Colors.white),
+                                      ],
+                                    )),
+                                MyButton(
+                                  'Spielen',
+                                  onPressed: () => {
+                                    if (nameFilld)
+                                      {
+                                        context.read<CharacterProvider>().setPlayerName(playerName),
+                                        context.pushAndRemoveUntil(Routes.game)
+                                      }
+                                    else
+                                      {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(SnackBar(
+                                          content: Text('Bitte gib ein Name ein'),
+                                        ))
+                                      }
+                                  },
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  );
+                }),
+              ),
             ),
           ),
         ),
       ),
-    ),
     );
   }
 }
